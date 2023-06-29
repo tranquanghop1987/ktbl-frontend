2cbdde599c6f40c4799a8e2ff067476022f275ad
LDAP\tqhop <tqhop@cmcglobal.vn>
2023-06-29 11:37:49 +0700

    Update FE

--- src/app/shared/widgets/Vision/index.tsx
+++ src/app/shared/widgets/Vision/index.tsx
@@ -1,9 +1,14 @@
 'use client';
 import { Button } from 'antd';
-import styles from './styles.module.css';
+import styles from './styles.module.scss';
+import { useInView } from 'react-intersection-observer';
 function Vision() {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    rootMargin: '-400px 0px',
+  });
   return (
-    <div className={styles.vision}>
+    <div className={styles.vision + ` w-full ${inView ? ` ${styles.animation}` : ` ${styles.animationOut}`}`} ref={ref}>
       <div className={styles.text}>
         <div className={styles.title}>
           <h2 className={styles.firstTitle}>GROWING TOGETHER</h2>
@@ -17,6 +22,12 @@ function Vision() {
           เกี่ยวกับบริษัท
         </Button>
       </div>
+      {/* <Image src="/assets/images/vision-left.png" width={500} height={500} alt="Picture of the author" />
+      <Image src="/assets/images/vision-right.png" width={500} height={500} alt="Picture of the author" />
+      <Image src="/assets/images/Vector 5.png" width={500} height={500} alt="Picture of the author" /> */}
+
+      <div className={styles.visionBackgroundLeft}></div>
+      <div className={styles.visionBackgroundRight}></div>
     </div>
   );
 }
